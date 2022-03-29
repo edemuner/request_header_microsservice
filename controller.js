d2741fd88c781baba0bd18e06a54ba6050a4ac08
edemuner <eduardood15@hotmail.com>
2022-03-29 12:39:06 -0300

    address was written wrong

--- controller.js
+++ controller.js
@@ -4,12 +4,13 @@ module.exports = app => {
 
     app.get('/api/whoami', (req, res) => {
 
+
         const ip = (req.ip).split(':')
         const ipv4 = ip[ip.length - 1]
 
         res.status(200).json(
             {
-                ipadress:ipv4,
+                ipaddress:ipv4,
                 language:req.get('accept-language'),
                 software:req.get('User-Agent')
             }
